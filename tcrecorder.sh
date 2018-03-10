752dec86b5d989ddaa539cc1030729ff3047403a
Etherrrr <20717049+Etherrrr@users.noreply.github.com>
2018-03-11 02:13:55 +0800

    Upload the first version

---
+++ tcrecorder.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+user=$1
+current=`date "+[%Y-%m-%d %H:%M:%S]"`
+tar_url=`curl -s https://twitcasting.tv/$1/metastream.m3u8 | grep ts-220.m3u8`
+filename=$user$(echo -n "_")
+output_format=ts
+output_dir=/root/tc/$user
+sleep_time=5
+
+if [ ! -d $output_dir ]; then
+	mkdir $output_dir
+fi
+
+while :
+do
+	curl -s https://twitcasting.tv/$user/metastream.m3u8 | grep -q ts-220.m3u8 
+	if [ $? -eq 0 ]; then
+		echo -n `date "+[%Y-%m-%d %H:%M:%S]"`
+		echo " Casting! Start to record!"
+		ffmpeg -i $tar_url $output_dir/$filename$(date "+%Y%m%d-%H:%M:%S").$output_format
+		echo -n `date "+[%Y-%m-%d %H:%M:%S]"`
+		echo " Recording finished!"
+	else
+		echo -n `date "+[%Y-%m-%d %H:%M:%S]"`
+		echo " Not casting!"
+	fi
+	sleep $sleep_time
+done
+
